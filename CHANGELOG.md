3aa6876ad1fc62328f13a7d60da0b9ee971ccaba
wangfan <wangfan@wangfan.wangfan>
2019-04-22 14:00:13 +0800

    Change the Tianyi Endpoint Api Address: `device.api.ct10649.com` replace the test address `180.101.147.89`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0 (2019-04-22)
+
+Change the Tianyi Endpoint Api Address: `device.api.ct10649.com` replace the test address `180.101.147.89`.
+
 ## 0.0.1
 
 Change `script` in `package.json`
--- kit/setting.js
+++ kit/setting.js
@@ -1,15 +1,11 @@
-const HOST = '180.101.147.89',
-      PORT = 8743,
-      ID = 'XdbNkfbFSRYPJhs4H2EJ4G12w6Aa',
-      SECRET = 'N5fUGGNl9aJIDapXNVkI3SVIfCYa';
+const HOST = 'device.api.ct10649.com',  // 180.101.147.89 is the test endpoint
+      PORT = 8743;
 
 const URL_PERFIX = `https://${HOST}:${PORT}/iocm/app`;
 
 module.exports = {
   host: HOST,
   port: PORT,
-  appId: ID,
-  secret: SECRET,
   secBaseUrl: `${URL_PERFIX}/sec/v1.1.0/`,
   cmdBaseUrl: `${URL_PERFIX}/cmd/v1.4.0/`,
   subBaseUrl: `${URL_PERFIX}/sub/v1.2.0/`,
